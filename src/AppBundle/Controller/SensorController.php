db10658954edb780f63a381e40f69440154d19df
Miroufff <sylvain.davenel@laposte.net>
2017-06-23 09:05:02 +0200

    Add update status action

---
+++ src/AppBundle/Controller/SensorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Sensor;
+use AppBundle\Form\SensorType;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\View\View as FOSView;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Voryx\RESTGeneratorBundle\Controller\VoryxController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class SensorController extends Controller
+{
+    /**
+     * @param Request $request
+     * @param Sensor $sensor
+     *
+     * @return JsonResponse
+     */
+    public function updateStatusAction(Request $request, Sensor $sensor) {
+        $sensor->setEnable(!$sensor->getEnable());
+        $this->getDoctrine()->getManager()->flush();
+
+        return new JsonResponse();
+    }
+}
